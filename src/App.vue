f610a3e7ec023cf67d2a22d496e317fbcc7e7953
geric <gericlois.casugay@lorma.edu>
2021-02-09 14:57:07 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <HelloWorld/>
+    <Department/>
+  </div>
+
+  
+</template>
+
+<script>
+import Department from './components/Department.vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: { HelloWorld, Department
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/Department.vue
@@ -0,0 +1,30 @@
+<template>
+   <div class="flex justify-center h-screen">
+    <div class="flex justify-col">
+      <div class="p-3 flex justify-center flex-col">
+        <div class="p-3 px-3 flex flex-col items-start border-collapse border border-black-80">
+          <label class="font-mono" for="department"> Department </label>
+          <input class="p-1 border-collapse border border-black-800" type="text"/>
+        </div>
+        <div class="p-2 px-3 flex flex-col items-end bg-gray-100">
+          <button class="p-1 my-2 font-mono bg-blue-900 text-white rounded-md focus:outline-none focus:ring-2 ring-blue-200 ring-offset-2" >
+            Create Department
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Department',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style>
+
+</style>
